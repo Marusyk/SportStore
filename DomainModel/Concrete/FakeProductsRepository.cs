bd83507b7631056badfd5d954d553352ed72c990
Roma <romamarusyk@gmail.com>
2015-03-05 00:23:20 +0200

    Recreate DomainModel

---
+++ DomainModel/Concrete/FakeProductsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel.Abstract;
+using DomainModel.Entities;
+
+namespace DomainModel.Concrete
+{
+    public class FakeProductsRepository : IProductsRepository
+    {
+        private static IQueryable<Product> fakeProducts = new List<Product>{
+            new Product { Name = "Football", Price = 25 },
+            new Product { Name = "Surf board", Price = 179 },
+            new Product { Name = "Running shoes", Price = 95 },
+        }.AsQueryable();
+
+        public IQueryable<Product> Products
+        {
+            get { return fakeProducts; }
+        }
+    }
+}
---
+++ DomainModel/Concrete/SqlProductsRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel.Abstract;
+using DomainModel.Entities;
+using System.Data.Linq;
+
+namespace DomainModel.Concrete
+{
+    public class SqlProductsRepository : IProductsRepository
+    {
+        private Table<Product> productTable;
+
+        public SqlProductsRepository(string connectionString)
+        {
+            productTable = (new DataContext(connectionString)).GetTable<Product>();
+        }
+
+        public IQueryable<Product> Products
+        {
+            get { return productTable; }
+        }
+    }
+}
