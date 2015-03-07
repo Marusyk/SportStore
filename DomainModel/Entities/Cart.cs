3a29c208f2d42ca6fc2b58b06cc829e2582332a6
Roma <romamarusyk@gmail.com>
2015-03-07 17:56:39 +0200

    Added model Cart. Chnaged web.config.

---
+++ DomainModel/Entities/Cart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> lines = new List<CartLine>();
+        public IList<CartLine> Lines { get { return lines; } }
+        public void AddItem(Product product, int quantity) { }
+        public decimal ComputeTotalValue()
+        {
+            throw new NotImplementedException();
+        }
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
