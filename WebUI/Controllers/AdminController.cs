1ef8861f7898cfd7b168f6331e3007b5dcf9d213
Roma <romamarusyk@gmail.com>
2015-03-12 23:44:21 +0200

    AdminController

--- WebUI/Controllers/AdminController.cs
+++ WebUI/Controllers/AdminController.cs
@@ -16,5 +16,48 @@ namespace WebUI.Controllers
             return View();
         }
 
+        public ActionResult Details(int id)
+        {
+            return View()
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Логіка вставки
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Login edit
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
     }
 }
