f11a4c9d4aeca032ee0b714513517579ba734440
Roma <romamarusyk@gmail.com>
2015-03-12 23:49:40 +0200

    AdminController

--- WebUI/Controllers/AdminController.cs
+++ WebUI/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DomainModel.Abstract;
 
 namespace WebUI.Controllers
 {
@@ -11,53 +12,16 @@ namespace WebUI.Controllers
         //
         // GET: /Admin/
 
+        private IProductsRepository productRepository;
+
+        public AdminController(IProductsRepository prodRepos)
+        {
+            productRepository = prodRepos;
+        }
+
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult Details(int id)
-        {
-            return View()
-        }
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Логіка вставки
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Login edit
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
     }
 }
