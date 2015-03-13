f355c8f3d57664d42b4ff453d8386b1db54cfd5f
Roma <romamarusyk@gmail.com>
2015-03-13 23:26:37 +0200

    Added view Edit

--- WebUI/Controllers/AdminController.cs
+++ WebUI/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DomainModel.Abstract;
+using DomainModel.Entities;
 
 namespace WebUI.Controllers
 {
@@ -22,6 +23,15 @@ namespace WebUI.Controllers
         public ViewResult Index()
         {
             return View(productRepository.Products.ToList());
+        }
+
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ViewResult Edit(int productID)
+        {
+            Product product = (from p in productRepository.Products
+                               where p.ProductID == productID
+                               select p).First();
+            return View(product);
         }
     }
 }
