112d970c3c1d32af6df76582a0cb187f11abd8a2
Roma <romamarusyk@gmail.com>
2015-03-20 23:23:51 +0200

    Added Image Tests

---
+++ Tests/ImageTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using DomainModel.Entities;
+using DomainModel.Abstract;
+using WebUI.Controllers;
+using System.Web.Mvc;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ImageTests
+    {
+        [Test]
+        public void Can_Retrieve_Image_Data()
+        {
+            Product prod = new Product
+            {
+                ProductID = 2,
+                Name = "Test",
+                ImageData = new byte[] { },
+                ImageMimeType = "image/png"
+            };
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product { ProductID = 1, Name = "P1" },
+                prod,
+                new Product { ProductID = 3, Name = "P3" }}.AsQueryable());
+            
+            ProductsController controller = new ProductsController(mock.Object);
+
+            ActionResult result = controller.GetImage(2);
+
+            Assert.IsNotNull(result);
+          //  Assert.IsInstanceOfType(result, typeof(FileResult));
+            Assert.AreEqual(prod.ImageMimeType, ((FileResult)result).ContentType);
+        }
+    }
+}
